2750d54e70948fafd1e116c71005130a1dea4b09
riffki <riffkikris13@gmail.com>
2026-03-30 14:03:40 +0700

    fix: Change module name to specific module

--- silverplast/api/qc_process.py
+++ silverplast/api/qc_process.py
@@ -4,6 +4,9 @@ import frappe
 def create_qc_process(incoming_qc):
     source = frappe.get_doc("Incoming QC", incoming_qc)
 
+    if frappe.db.exists("QC Process", {"source_incoming_qc": source.name}):
+        frappe.throw("QC Process sudah dibuat untuk data ini")
+
     qc = frappe.get_doc({
         "doctype": "QC Process",
         "source_incoming_qc": source.name,
